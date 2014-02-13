0abd83c0183bc12f0eeb8dc1974054285fa340ff
Russell Matsuo <design@russellmatsuo.com>
2014-02-12 18:24:55 -0800

    improvements to pdf viewer

--- app/assets/javascripts/lapsity.js
+++ app/assets/javascripts/lapsity.js
@@ -5,6 +5,10 @@
 $(function () {
   'use strict';
   var sound_active = false,
+    current_pdf_size,
+    viewer_open = false,
+    called_once = false,
+    debounce_scroll,
 
   replace_images = function (targets) {
     $.each(targets, function () {
@@ -23,10 +27,40 @@ $(function () {
 
   size_gallery = function() {
     var current_width = $("#pdf_headline").width();
-    var current_height = current_width * (1/1.83);
-    $("#iframe_protector").css("height", current_height);
-    $("#google_viewer").css("height", current_height);
-    $("#pdf_viewer").css("height", current_height + 30);
+    current_pdf_size = current_width * (1/1.83);
+    $("#iframe_protector").css("height", current_pdf_size);
+    $("#google_viewer").css("height", current_pdf_size);
+    $("#pdf_viewer").css("height", current_pdf_size + 30);
+
+  }, 
+
+  open_viewer = function() {
+    $("#google_viewer").css("height", $(window).height() - $("#global_header").height() - 30);
+    $("#pdf_viewer").css("height", $(window).height() - $("#global_header").height());
+    var new_scrolltop = $("#pdf_headline").offset().top - $("#global_header").height();
+    $("#user-study").css({width: "95%"});
+    $("html, body").animate({scrollTop: new_scrolltop}, 400, function(){
+      if(!called_once){
+        setTimeout(function() {
+          viewer_open = true;
+        }, 100);
+        called_once = true;
+      }
+    });
+  }, 
+
+  close_viewer = function() {
+    var current_width = $("#pdf_headline").width();
+    current_pdf_size = current_width * (1/1.83);
+    $("#iframe_protector").animate({height: current_pdf_size}, 400);
+    $("#google_viewer").animate({height: current_pdf_size}, 400);
+    $("#pdf_viewer").animate({height: current_pdf_size + 30}, 400);
+    $("#iframe_protector").fadeIn();
+    $("#view_options").fadeIn();
+    $("#user-study").css({width: "80%"});
+    $("#google_viewer").addClass("blurred");
+    viewer_open = false;
+    called_once = false;
   };
 
   $(document).ready(function() {
@@ -38,11 +72,18 @@ $(function () {
       $("#iframe_protector").fadeOut();
       $("#view_options").fadeOut();
       $("#google_viewer").removeClass("blurred");
+      open_viewer();
     });
   });
 
   $(window).resize(function() {
     size_demo();
     size_gallery();
+  });
+
+  $(window).scroll(function() {
+    if (viewer_open) {
+      close_viewer();
+    }
   });
 });
